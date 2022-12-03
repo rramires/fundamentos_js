d7cebff9860e77a4a7a0accaafb92cf28616af41
rramires <ricardo@flexbr.com>
2022-12-03 18:01:02 -0300

    Fundamentos do Express, adiciondo mook de cadastro

--- 01_Express_Intro/routes/products.js
+++ 01_Express_Intro/routes/products.js
@@ -1,17 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// Mook data
+global.products = [{category: "Info", 
+                    name: 'Notebook',
+                    price: 5000},
+                   {category: "Info",
+                    name: 'Celular',
+                    price: 2000},
+                   {category: "Info",
+                    name: 'Webcam',
+                    price: 400},
+                   {category: "Office",
+                    name: 'Paper',
+                    price: 50}];
+
+global.categories = ["Info", "Office"];
+
+/* GET products */
 router.get('/', function(req, res, next) {
-  res.render('products', { title: 'Products',
-                        products: [
-                                {name: 'Notebook',
-                                 price: 5000},
-                                {name: 'Celular',
-                                 price: 2000},
-                                {name: 'Webcam',
-                                 price: 400}
-                        ]});
+  res.render('products', { title: 'Products', products:  global.products });
 });
+
+
+/* GET add form */
+router.get('/add', function(req, res, next) {
+  res.render('addproducts', { title: 'Add Products', products:  global.products });
+});
+
+
+/* ADD products */
+router.post('/', function(req, res, next) {
+  // insert
+  global.products.push({ name: req.body.name, price: req.body.price, category: req.body.category });
+  // return to prducts
+  res.render('products', { title: 'Products', products:  global.products });
+});
+
 
 module.exports = router;
