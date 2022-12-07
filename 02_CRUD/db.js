e1b6142241b3b90bba6bee8955664c27e6c35afd
rramires <ricardo@flexbr.com>
2022-12-06 22:25:50 -0300

    CRUD, refact conexão promise

--- 02_CRUD/db.js
+++ 02_CRUD/db.js
@@ -1,17 +1,12 @@
 const mongoClient = require('mongodb').MongoClient;
 mongoClient.connect("mongodb://localhost:27017", 
-                    { useUnifiedTopology: true },
-                    (error, connection) => {
-                        // se tiver erro imprime
-                        if(error){
-                            console.log(error);
-                        } 
-                        else{
-                            // senao seta como global e seleciona o banco
-                            global.dbconn = connection.db('crud');
-                            console.log('MongoDB connected!');
-                        }
-                    });
+                    { useUnifiedTopology: true })
+                    .then(connection => {
+                        // senao seta como global e seleciona o banco
+                        global.dbconn = connection.db('crud');
+                        console.log('MongoDB connected!');
+                    })
+                    .catch(error => console.log(error));
 
 
 function findCustomers(){
