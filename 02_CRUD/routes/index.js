f3e1a919dc50f9264a4ec24ee7387aadbb86b6c2
rramires <ricardo@flexbr.com>
2022-12-08 16:00:05 -0300

    CRUD, customer insert

--- 02_CRUD/routes/index.js
+++ 02_CRUD/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { AggregationCursor } = require('mongodb');
 const router = express.Router();
 const db = require('../db');
 
@@ -13,10 +14,31 @@ router.get('/', function(req, res, next) {
                       .catch(error => console.log(error));
 });
 
-/* GET home page. */
+
+/* GET new customer page. */
 router.get('/new', function(req, res, next) {
   // 
   res.render('customer', { title: "Customer Registration" });
+});
+
+
+/* GET subimited data. */
+router.post('/new', function(req, res, next) {
+  // 
+  const name = req.body.name;
+  const age = parseInt(req.body.age);
+  const state = req.body.state;
+  // Validation
+  name || res.redirect('/new?error=Name cannot be empty!');
+  age && /[0-9]+/.test(age) || res.redirect('/new?error=Age must be a number!');
+  state.length === 2 || res.redirect('/new?error=Select a state!');
+  //
+  const custumers = db.insertCostumer( { name, age, state } )
+                      .then(result => {
+                        // 
+                        res.redirect('/');
+                      })
+                      .catch(error => console.log(error));
 });
 
 module.exports = router;
