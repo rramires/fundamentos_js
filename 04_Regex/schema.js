6705702b13c8637b76aed12d22ce42652d6e47cd
rramires <ricardo@flexbr.com>
2022-12-18 18:02:36 -0300

    Regex - Basic example

--- 04_Regex/schema.js
+++ 04_Regex/schema.js
@@ -1,5 +1,18 @@
 // docs: https://joi.dev/api/
 const Joi = require('joi');
+
+/*
+ RegEx no Javascript 
+ Pattern delimitado por barras:
+ /pattern/flags .exec() ou .test()
+
+ ex: retorna um array de resultados
+const arrResult = /abc/ig.exec();
+
+ ex: retorna um booleano
+const isValid = /abc/i.test();
+ */
+
 //
 const schema = Joi.object({
     username: Joi.string()
